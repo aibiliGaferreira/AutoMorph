1cabe40c53f5f9ba55810d9739791b35c6a14bce
aibiliGaferreira <gaferreira@aibili.pt>
2026-03-26 10:16:21 +0000

    updated tests

--- M1_Retinal_Image_quality_EyePACS/dataset.py
+++ M1_Retinal_Image_quality_EyePACS/dataset.py
@@ -101,6 +101,9 @@ class BasicDataset_OUT(Dataset):
         elif isinstance(imgs, list) and all(isinstance(i, str) for i in imgs): # List of file paths
             self.ids = imgs
             self.imgs = [np.array(Image.open(file)) for file in imgs]
+        elif isinstance(imgs, np.ndarray) and imgs.ndim == 3: # Single memory image
+            self.ids = ["0"]
+            self.imgs = [imgs]
         else: # List of images
             self.ids = list(map(str, range(len(imgs))))
             self.imgs = imgs
